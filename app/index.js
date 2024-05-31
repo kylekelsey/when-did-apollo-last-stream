c8f1592cbc7d624c0a57261648103595ea17bb1a
Kyle Kelsey <toughtigerfist26@gmail.com>
2024-05-31 10:04:58 -0400

    cert changes

--- app/index.js
+++ app/index.js
@@ -3,14 +3,14 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT;
 const cron = require("./cron");
-const db = require('./db');
+const db = require("./db");
 const logger = require("./logger");
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "static/index.html");
 });
 
-app.use("/static", express.static(__dirname + "/static"));
+app.use(express.static("static"));
 
 app.get("/api", async (req, res) => {
   const client = await db.getClient();
@@ -18,7 +18,7 @@ app.get("/api", async (req, res) => {
   try {
     const res = await client.query("SELECT * from last_stream");
     lastStream = res.rows[0].laststream;
-    isLive = res.rows[0].islive
+    isLive = res.rows[0].islive;
   } catch (err) {
     console.error(err.message);
   } finally {
